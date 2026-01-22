4ba3afbd06568f7c1a96400e226bb99baea2a98c
Teejayskillz <teejayadewale87@gmail.com>
2026-01-22 21:11:28 +0100

    registered d admin

--- backend/students/admin.py
+++ backend/students/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Course, ClassSession, Attendance
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(ClassSession)
+admin.site.register(Attendance)
